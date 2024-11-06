65c79bcf202bca8b3d781a7719438058e95d31ba
Musny Mubarak <119862882+musnymubarak@users.noreply.github.com>
2024-11-06 23:05:00 +0530

    Delete server directory

--- server/Models/Semester.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const semesterSchema = new mongoose.Schema({
-  name: String,
-  courses: [{ name: String, credits: Number }],
-});
-
-module.exports = mongoose.model('Semester', semesterSchema);
